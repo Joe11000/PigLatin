b95a50d8e25c26bb160052f1f0fae21995ff6d8c
Joe Noonan <joenoonan27@gmail.com>
2014-08-14 13:17:41 -0500

    added README and now accepts ARGV input

--- Pig-latin_solution.rb
+++ Pig-latin_solution.rb
@@ -1,6 +1,8 @@
+a = ARGV[0..-1].join(" ")  #"How is life"
+
 def convert_word(word)
-  match_results = /(\A[^aeiou]*)(.*)/i.match(word) 
-  if match_results[1].length > 0 
+  match_results = /(\A[^aeiou]*)(.*)/i.match(word)
+  if match_results[1].length > 0
     match_results[2] + match_results[1] + "ay"
   else
     match_results[2]
@@ -11,8 +13,5 @@ def convert_sentence(sentence)
   sentence.split(" ").map{|i| convert_word(i)}.join(" ")
 end
 
-a = "How is life"
 
 puts convert_sentence(a)
-
-puts "a : #{a}"
---
+++ README.txt
@@ -0,0 +1,5 @@
+Run Application
+  ie
+    ruby Pig-latin_solution.rb my sentence here
+
+         myay entencesay erehay # output
